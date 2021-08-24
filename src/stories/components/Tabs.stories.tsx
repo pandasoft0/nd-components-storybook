5335991a5393bea7ef452f28eb5fa3f40f6b7d29
= <=>
2021-08-24 13:10:56 -0300

    fix: initial commit

--- src/stories/components/Tabs.stories.tsx
+++ src/stories/components/Tabs.stories.tsx
@@ -7,12 +7,15 @@ export default {
   component: Tab
 } as ComponentMeta<typeof Tab>;
 
-export const Default = () => (
-  <Tab>
-    <Tab.Item label="Head">a</Tab.Item>
-    <Tab.Item label="Body">b</Tab.Item>
-    <Tab.Item label="Arms">c</Tab.Item>
-    <Tab.Item label="Legs">d</Tab.Item>
-    <Tab.Item label="Weapons">e</Tab.Item>
-  </Tab>
-);
+const panes = [
+  {
+    menuItem: 'Head',
+    render: () => <></>
+  },
+  { menuItem: 'Body' },
+  { menuItem: 'Arms' },
+  { menuItem: 'Legs' },
+  { menuItem: 'Weapons' }
+];
+
+export const Default = () => <Tab panes={panes} />;
