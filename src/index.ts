aa0ad72d8a5d9b5888aea39b2a0a48cce519f864
= <=>
2021-09-28 10:40:02 -0300

    feat: initial component struct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Breadcrumb from './components/Breadcrumbs';
-import Alert from './components/Alert';
 import Button from './components/Button';
 import Card from './components/Card';
 import Modal from './components/Modal';
@@ -7,4 +6,4 @@ import Steps from './components/Steps';
 import Tab from './components/Tab';
 import ThemeProvider from './components/ThemeProvider';
 
-export { Breadcrumb, Alert, Button, Card, Modal, Steps, Tab, ThemeProvider };
+export { Breadcrumb, Button, Card, Modal, Steps, Tab, ThemeProvider };
